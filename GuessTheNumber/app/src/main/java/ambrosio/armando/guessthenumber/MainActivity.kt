32c85cdc719dde4d359adf91c617effecea51023
ArmandoAmbrosioS <armando.ambrosiosoto@gmail.com>
2021-06-12 00:23:03 -0700

    Finished

--- GuessTheNumber/app/src/main/java/ambrosio/armando/guessthenumber/MainActivity.kt
+++ GuessTheNumber/app/src/main/java/ambrosio/armando/guessthenumber/MainActivity.kt
@@ -2,11 +2,83 @@ package ambrosio.armando.guessthenumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    var minValue=0
+    var maxValue=100
+    var num: Int=0
+    var won: Boolean=false
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Furro el que lo lea")
+
+        val guessings: TextView = findViewById(R.id.guessings)
+        val down: Button =findViewById(R.id.down)
+        val up: Button = findViewById(R.id.up)
+        val generate: Button = findViewById(R.id.generate)
+        val guessed: Button = findViewById(R.id.guess)
+
+        generate.setOnClickListener(){
+            num= Random.nextInt(minValue,maxValue)
+            guessings.setText(num.toString())
+            generate.visibility= View.INVISIBLE
+            guessed.visibility= View.VISIBLE
+        }
+
+        up.setOnClickListener(){
+
+            minValue= num
+            if (checkingLimits()) {
+                num = Random.nextInt(minValue, maxValue)
+                guessings.setText(num.toString())
+            }else{
+                guessings.setText("No puede ser, me has vencido")
+            }
+        }
+
+        down.setOnClickListener(){
+            maxValue=num
+            if (checkingLimits()) {
+                num = Random.nextInt(minValue, maxValue)
+                guessings.setText(num.toString())
+            }else{
+                guessings.setText("No puede ser, me has vencido")
+            }
+        }
+
+        guessed.setOnClickListener(){
+            if (!won){
+                guessings.setText("Adivine, tu numero es el "+num)
+                guessed.setText("Volver a jugar")
+                won=true
+            }else{
+                generate.visibility= View.VISIBLE
+                guessings.setText("Tap on GENERATE to start")
+                guessed.visibility= View.GONE
+                resetValues()
+            }
+        }
+
     }
+
+    fun checkingLimits(): Boolean{
+        return minValue != maxValue
+    }
+
+    fun resetValues(){
+        var minValue=0
+        var maxValue=100
+        var num: Int=0
+        var won: Boolean=false
+    }
+
+
 }
